df7b7c89b8469974c379acb8c847fcd93869af94
frajusu <faustogonzalez1510@hotmail.com>
2025-10-07 19:14:13 -0300

    Agregar nuevo contenido

---
+++ Scripts/Ejecutador.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+
+
+func ejectutar_jugada(nodo):
+#	if !get_tree().root.has_node("Game"):
+#		return
+#	get_tree().root.get_node("Game").baraja_activa = "ejecutando"
+	
+	recorrer_domino(nodo)
+
+
+func recorrer_domino(d: Node) -> void:
+	print(d.nombre, "        ", d.global_position)  # imprime el nombre del domino actual
+	
+	# buscar todos los slots dentro de este domino
+	for slot in d.get_slots_in_self():
+		if slot.hijo != null:
+			recorrer_domino(slot.hijo)  # recursion
